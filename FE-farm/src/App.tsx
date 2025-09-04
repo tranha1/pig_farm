34ee6186b1efa762c7252b377e7ba68d68f1d2f0
quannh08 <quan.nguyen.0008@gmail.com>
2025-09-04 15:58:47 +0700

    add product page

--- FE-farm/src/App.tsx
+++ FE-farm/src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound";
 import About from "./pages/About.tsx";
+import Products from "./pages/Products.tsx";
 
 const queryClient = new QueryClient();
 
@@ -18,6 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
