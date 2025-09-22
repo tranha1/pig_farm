eb7d5ca87bbff38557b282f176cf7d738c74ac86
Mai Danh Hieu <danhhieu123ns@gmail.com>
2025-09-22 10:09:37 +0700

    delete django

--- BE-farm/pig_farm/core/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
-    
-    def ready(self):
-        """Import signals when app is ready"""
-        import core.signals  # This registers the signal handlers
--- BE-farm/pig_farm/core/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from .news_models import NewsIndexPage, NewsPage
-
-# Create your models here.
-
-# Import Wagtail page models to register them
-__all__ = ['NewsIndexPage', 'NewsPage']
